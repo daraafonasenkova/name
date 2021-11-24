0d3b3deab6e437cdb350e362a956a1f7189c5818
Дарья <daraafonasenkova74@gmail.com>
2021-11-24 19:37:51 +0300

    2

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+import sys
+from random import randint
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt5 import uic
+from PyQt5.QtCore import Qt, QPoint
+from PyQt5.QtGui import QPainter, QColor, QPolygon
+from PyQt5.QtWidgets import QWidget, QApplication
 
 
-# Press the green button in the gutter to run the script.
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('tt.ui', self)
+        self.pushbutton.clicked.connect(self.run)
+        self.shape = 1
+        #self.do_paint = False
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            print(self.coords)
+            x, y = self.coords
+            colors = (randint(0, 255) for i in range(3))
+            qp.setBrush(QColor(*colors))
+            if self.shape == 1:
+                w = randint(10, 100)
+                qp.drawEllipse(x - w // 2, y - w // 2, w, w)
+
+    def run(self, event):
+        if event.button() == Qt.LeftButton:
+            self.shape = 1
+
+    def mouseMoveEvent(self, event):
+        self.coords = [event.x(), event.y()]
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
