ff8ac03eb0c97b0deee5f648e3397228e2b38453
Дарья <daraafonasenkova74@gmail.com>
2021-11-24 21:23:43 +0300

    3

--- main.py
+++ main.py
@@ -4,35 +4,34 @@ from random import randint
 from PyQt5 import uic
 from PyQt5.QtCore import Qt, QPoint
 from PyQt5.QtGui import QPainter, QColor, QPolygon
-from PyQt5.QtWidgets import QWidget, QApplication
+from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
 
 
-class Example(QWidget):
+class Example(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('tt.ui', self)
-        self.pushbutton.clicked.connect(self.run)
+        self.pushButton.clicked.connect(self.run)
         self.shape = 1
-        #self.do_paint = False
+        self.do_paint = False
 
     def paintEvent(self, event):
         if self.do_paint:
             qp = QPainter()
             qp.begin(self)
-            print(self.coords)
-            x, y = self.coords
-            colors = (randint(0, 255) for i in range(3))
+            x = randint(10, 400)
+            y = randint(10, 400)
+            colors = (255, 255, 0)
             qp.setBrush(QColor(*colors))
             if self.shape == 1:
                 w = randint(10, 100)
                 qp.drawEllipse(x - w // 2, y - w // 2, w, w)
+            qp.end()
 
-    def run(self, event):
-        if event.button() == Qt.LeftButton:
-            self.shape = 1
-
-    def mouseMoveEvent(self, event):
-        self.coords = [event.x(), event.y()]
+    def run(self):
+        self.shape = 1
+        self.do_paint = True
+        self.repaint()
 
 
 if __name__ == '__main__':
